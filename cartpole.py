1c4532971907f16cf0a7f76d8e4044058c88fbd2
ZhengTzer <zhengtzer@gmail.com>
2019-11-03 02:18:33 +0800

    Add files via upload

---
+++ cartpole.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+import gym
+import random
+import numpy as np
+from collections import deque
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.optimizers import Adam
+from mish import Mish
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+# Environment
+env = gym.make('CartPole-v1')
+state_size = env.observation_space.shape[0]
+action_size = env.action_space.n
+
+# Seed
+seed = 10
+env.seed(seed)
+random.seed(seed)
+np.random.seed(seed)
+
+
+# In[3]:
+
+
+# Training parameterss
+discount_factor = 0.99
+epsilon_decay = 0.995
+epsilon_min = 0.01
+batch_size = 32
+train_start = 1000
+memory_size = 10000
+n_episodes = 3000
+n_win_ticks = 195
+n_avg_scores = 100
+render = False
+
+
+# In[4]:
+
+
+# Build model
+def build_model():
+    model = Sequential()
+    model.add(Dense(96, input_dim=state_size, kernel_initializer='he_uniform'))
+    model.add(Mish())
+    model.add(Dense(48, kernel_initializer='he_uniform'))
+    model.add(Mish())
+    model.add(Dense(24, kernel_initializer='he_uniform'))
+    model.add(Mish())
+    model.add(Dense(action_size, kernel_initializer='he_uniform'))
+    model.compile(Adam(lr=0.001), loss='mse')
+    return model
+
+model = build_model()
+target_model = build_model()
+
+model.summary()
+
+
+# In[5]:
+
+
+def update_target_model():
+    target_model.set_weights(model.get_weights())
+
+def get_action(state, epsilon):
+    return np.random.randint(action_size) if np.random.rand() <= epsilon else np.argmax(model.predict(state)[0])
+
+def train_replay():
+    if len(memory) < train_start:
+        return
+    minibatch = random.sample(memory,  min(batch_size, len(memory)))
+    state_t, action_t, reward_t, state_t1, terminal = zip(*minibatch)
+    state_t = np.concatenate(state_t)
+    state_t1 = np.concatenate(state_t1)
+    targets = model.predict(state_t)
+    Q_sa = target_model.predict(state_t1)
+    targets[range(batch_size), action_t] = reward_t + discount_factor * np.max(Q_sa, axis=1) * np.invert(terminal)
+    model.train_on_batch(state_t, targets)
+
+
+# In[6]:
+
+
+# Training
+avg_scores = []
+all_scores = []
+scores = deque(maxlen=n_avg_scores)
+memory = deque(maxlen=memory_size)
+
+def learn_to_balance():
+    epsilon = 1.0
+    has_won = False
+
+    for e in range(n_episodes):
+        done = False
+        score = 0
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+
+        while not done:
+            action = get_action(state, epsilon)
+            next_state, reward, done, info = env.step(action)
+            next_state = np.reshape(next_state, [1, state_size])
+
+            memory.append((state, action, reward if not done else -100, next_state, done))
+            if epsilon > epsilon_min:
+                epsilon *= epsilon_decay
+            train_replay()
+            score += reward
+            state = next_state
+            
+            if render:
+                env.render()
+
+            if done:
+                env.reset()
+                update_target_model()
+                scores.append(score)
+                all_scores.append(score)
+                avg_score = np.mean(scores)
+                avg_scores.append(avg_score)
+
+                if not has_won and e >= n_avg_scores and avg_score >= n_win_ticks:
+                    solution_episode_idx = max(next(x[0] for x in enumerate(all_scores) if x[1] >= n_win_ticks), e - n_win_ticks) 
+                    print('Solved after {} tries! \o/'.format(solution_episode_idx))
+                    has_won = True
+
+                if e % n_avg_scores == 0:
+                    print('[Episode {}] Average Score: {}'.format(e, avg_score))
+
+
+learn_to_balance()
+
+# Plot results
+plt.figure(figsize=(12,6))
+plt.plot(all_scores, color='blue')
+plt.plot(avg_scores, color='red')
+plt.show()
+
